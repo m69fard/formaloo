f55e58ef6893a9fa610822f2a17504ca7f362463
mehdi <m69fard@gmail.com>
2025-02-22 13:44:29 +0300

    Add AppAdmin

--- apps/appstore/admin.py
+++ apps/appstore/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import App
+
+
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "status", "created_at")
+    list_filter = ("status",)
+    actions = ["verify_apps"]
+
+    def verify_apps(self, request, queryset):
+        queryset.update(status="verified")
+
+    verify_apps.short_description = "Mark selected apps as verified"
