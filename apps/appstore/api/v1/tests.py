126c66d2efc1775d79e4937ccb7031d2471339f5
mehdi <m69fard@gmail.com>
2025-02-24 13:36:33 +0300

    Fix User in tests

--- apps/appstore/api/v1/tests.py
+++ apps/appstore/api/v1/tests.py
@@ -1,10 +1,12 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
 from apps.appstore.models import App
+
+User = get_user_model()
 
 
 class AppTests(APITestCase):
--- apps/appstore/tests.py
+++ apps/appstore/tests.py
@@ -1,8 +1,10 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from apps.appstore.models import App
+
+User = get_user_model()
 
 
 class TestAppAdmin(TestCase):
